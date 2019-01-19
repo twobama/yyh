db921c58195320c68c582490d08fbfc0ec5747ed
lm <779078499@qq.com>
2019-01-19 13:08:47 +0800

    发布项目

---
+++ YYH/src/main/java/com/hxtj/YyhApplication.java
@@ -0,0 +1,13 @@
+package com.hxtj;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YyhApplication.class, args);
+	}
+
+}
+
