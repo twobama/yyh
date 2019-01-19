35b1e80b8e5b695f7c3bd230bf99c09c3a1abdfc
twobama <39049618+twobama@users.noreply.github.com>
2019-01-19 13:29:41 +0800

    Update YyhApplicationTests.java

--- YYH/src/test/java/com/hxtj/YyhApplicationTests.java
+++ YYH/src/test/java/com/hxtj/YyhApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class YyhApplicationTests {
 
-	@Test//aaaa
+	@Test//aaaa111
 	public void contextLoads() {
 	}
 
